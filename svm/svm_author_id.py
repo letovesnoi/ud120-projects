84b9d285828619f424b7cb268e9e6b9ee99c887e
letovesnoi <lenk.ff@gmail.com>
2018-12-22 18:37:26 +0300

    Slice the training dataset down to 1% (lesson 3, SVM)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -29,6 +29,9 @@ from sklearn.metrics import accuracy_score
 
 clf = svm.SVC(kernel="linear")
 
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
+
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
