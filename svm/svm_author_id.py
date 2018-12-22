a1f2c76abbe73fa1921499051f8108adb5383ab7
letovesnoi <lenk.ff@gmail.com>
2018-12-22 18:53:09 +0300

    1% of training dataset, “rbf” SVM kernel, the best C value is 10000 (lesson 3, SVM)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,7 +27,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-clf = svm.SVC(kernel="rbf")
+clf = svm.SVC(kernel="rbf", C=10000.0)
 
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
