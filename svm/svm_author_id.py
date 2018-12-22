8db8fcd140cbf187a072fc3e1b6ac1158c49adb4
letovesnoi <lenk.ff@gmail.com>
2018-12-22 18:44:05 +0300

    1% of training dataset, “rbf” SVM kernel (lesson 3, SVM)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -27,7 +27,7 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-clf = svm.SVC(kernel="linear")
+clf = svm.SVC(kernel="rbf")
 
 features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
