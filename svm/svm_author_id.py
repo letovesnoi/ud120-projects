0e4a95d77a6349362407f453e1ff6b7dfe12cf49
letovesnoi <lenk.ff@gmail.com>
2018-12-22 18:26:47 +0300

    Machine Learning for Author ID not by NB but by SVM (lesson 3)

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+clf = svm.SVC(kernel="linear")
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+print "Accuracy:", round(accuracy_score(pred, labels_test), 2)
 
 #########################################################
 
