222ca90c474bec2a25a52d7933dfd06588970fef
letovesnoi <lenk.ff@gmail.com>
2018-12-29 23:40:43 +0300

    The total value of the stock belonging to James Prentice (lesson 6)

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,11 +19,10 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-person_of_interest = 0
-for lastname_firstname_middleinitial in enron_data:
-    print lastname_firstname_middleinitial
-    if enron_data[lastname_firstname_middleinitial]["poi"] == 1:
-        person_of_interest += 1
+# person_of_interest = 0
+# for lastname_firstname_middleinitial in enron_data:
+#     print lastname_firstname_middleinitial
+#     if enron_data[lastname_firstname_middleinitial]["poi"] == 1:
+#         person_of_interest += 1
 
-print person_of_interest
-
+print enron_data['PRENTICE JAMES']['total_stock_value']
