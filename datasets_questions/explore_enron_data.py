3171252a0901ea55784a01cfed41e53ff45296d3
letovesnoi <lenk.ff@gmail.com>
2018-12-30 21:57:38 +0300

    Number of quantified salary and known email address (lesson 6)

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,11 +20,20 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 # person_of_interest = 0
-# for lastname_firstname_middleinitial in enron_data:
+
+# How many folks in this dataset have a quantified salary? What about a known email address?
+quantified_salary = 0
+email_address = 0
+for lastname_firstname_middleinitial in enron_data:
 #     print lastname_firstname_middleinitial
 #     if enron_data[lastname_firstname_middleinitial]["poi"] == 1:
 #         person_of_interest += 1
-
+    if enron_data[lastname_firstname_middleinitial]['salary'] != 'NaN':
+        quantified_salary += 1
+    if enron_data[lastname_firstname_middleinitial]['email_address'] != 'NaN':
+        email_address += 1
+print 'Quantified salary: ', quantified_salary
+print 'Known email address: ', email_address
 # print enron_data['PRENTICE JAMES']['total_stock_value']
 # print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
 # print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
@@ -34,10 +43,9 @@ enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r")
 # Chairman of the Enron board of directors: Kenneth Lay
 # CFO (chief financial officer) of Enron: Andrew Fastow
 # Who took home the most money?
-
-money_lay = enron_data['LAY KENNETH L']['total_payments']
-money_skilling = enron_data['SKILLING JEFFREY K']['total_payments']
-money_fastow = enron_data['FASTOW ANDREW S']['total_payments']
-most_money = max(money_lay, money_skilling, money_fastow)
-print 'Lay money: ', money_lay, '\nSkilling money: ', money_skilling, '\nFastow money: ', money_fastow
-print '\nMost money: ', most_money
+# money_lay = enron_data['LAY KENNETH L']['total_payments']
+# money_skilling = enron_data['SKILLING JEFFREY K']['total_payments']
+# money_fastow = enron_data['FASTOW ANDREW S']['total_payments']
+# most_money = max(money_lay, money_skilling, money_fastow)
+# print 'Lay money: ', money_lay, '\nSkilling money: ', money_skilling, '\nFastow money: ', money_fastow
+# print '\nMost money: ', most_money
