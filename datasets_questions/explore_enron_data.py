528f1fff724bfcd8428856375ab94df4b9e13812
letovesnoi <lenk.ff@gmail.com>
2018-12-30 14:43:37 +0300

    Who took home the most money? (lesson 6)

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -27,4 +27,17 @@ enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r")
 
 # print enron_data['PRENTICE JAMES']['total_stock_value']
 # print enron_data['COLWELL WESLEY']['from_this_person_to_poi']
-print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+# print enron_data['SKILLING JEFFREY K']['exercised_stock_options']
+
+
+# CEO of Enron: Jeffrey Skilling
+# Chairman of the Enron board of directors: Kenneth Lay
+# CFO (chief financial officer) of Enron: Andrew Fastow
+# Who took home the most money?
+
+money_lay = enron_data['LAY KENNETH L']['total_payments']
+money_skilling = enron_data['SKILLING JEFFREY K']['total_payments']
+money_fastow = enron_data['FASTOW ANDREW S']['total_payments']
+most_money = max(money_lay, money_skilling, money_fastow)
+print 'Lay money: ', money_lay, '\nSkilling money: ', money_skilling, '\nFastow money: ', money_fastow
+print '\nMost money: ', most_money
