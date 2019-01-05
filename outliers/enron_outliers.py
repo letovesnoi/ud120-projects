4b2f77638ed19aefde82fa889a4c99a3916842a4
letovesnoi <lenk.ff@gmail.com>
2019-01-05 18:51:30 +0300

    The Biggest Enron Outlier is Total (lesson 8)

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -14,10 +14,17 @@ data = featureFormat(data_dict, features)
 
 
 ### your code below
+max_salary = 0
+max_bonus = 0
 for point in data:
     salary = point[0]
     bonus = point[1]
     matplotlib.pyplot.scatter( salary, bonus )
+    if salary > max_salary:
+        max_salary = salary
+    if bonus > max_bonus:
+        max_bonus = bonus
+print max_salary, max_bonus
 
 matplotlib.pyplot.xlabel("salary")
 matplotlib.pyplot.ylabel("bonus")
