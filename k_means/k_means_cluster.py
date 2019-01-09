8ae0bc39a6cf41aa3eab921a2cfcafc1781d49d8
letovesnoi <lenk.ff@gmail.com>
2019-01-09 20:24:54 +0300

    Computing rescaled features (lesson 10)

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,43 +48,46 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+# feature_3 = "total_payments"
 poi = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list)
 poi, finance_features = targetFeatureSplit(data)
 
+# Transforms features by scaling each feature to a given range
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+rescaled_finance_features = scaler.fit_transform(finance_features)
+print scaler.transform([[200000., 1000000.]])
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
+for f1, f2 in rescaled_finance_features:
     plt.scatter(f1, f2)
-    # plt.scatter(f1, f3)
-    # plt.scatter(f2, f3)
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 from sklearn.cluster import KMeans
 kmeans = KMeans(n_clusters=2)
-pred = kmeans.fit_predict(finance_features)
+pred = kmeans.fit_predict(rescaled_finance_features)
 
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, rescaled_finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
 
 
 # What are the maximum and minimum values of the feature_name
-feature_name = "salary"
-feature_data = [data_dict[name][feature_name] for name in data_dict
-                if data_dict[name][feature_name] != 'NaN']
-max_feature_value = max(feature_data)
-min_feature_value = min(feature_data)
-print 'max: ', max_feature_value, '\nmin: ', min_feature_value
+# feature_name = "salary"
+# feature_data = [data_dict[name][feature_name] for name in data_dict
+#                 if data_dict[name][feature_name] != 'NaN']
+# max_feature_value = max(feature_data)
+# min_feature_value = min(feature_data)
+# print 'max: ', max_feature_value, '\nmin: ', min_feature_value
