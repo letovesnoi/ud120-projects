6025e6eebc49655c0cb19c0618c39fb99518376b
letovesnoi <lenk.ff@gmail.com>
2019-01-11 21:10:26 +0300

    Decision tree, accuracy, 150 training points for overfitting (lesson 12)

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -19,25 +19,30 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
                              stop_words='english')
 features_train = vectorizer.fit_transform(features_train)
-features_test  = vectorizer.transform(features_test).toarray()
+features_test = vectorizer.transform(features_test).toarray()
 
 
 ### a classic way to overfit is to use a small number
 ### of data points and a large number of features;
 ### train on only 150 events to put ourselves in this regime
 features_train = features_train[:150].toarray()
-labels_train   = labels_train[:150]
+labels_train = labels_train[:150]
 
 
 
 ### your code goes here
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-
-
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "Accuracy:", accuracy_score(pred, labels_test)
+print 'Number of training points: ', len(features_train)
