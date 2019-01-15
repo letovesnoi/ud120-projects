055dd5c661468a363da24c2b3781cfa38d6e37f9
letovesnoi <lenk.ff@gmail.com>
2019-01-15 20:40:40 +0300

    Automatically tuned parameter values by GridSearchCV (lesson 14)

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -99,6 +99,7 @@ clf = clf.fit(X_train_pca, y_train)
 print "done in %0.3fs" % (time() - t0)
 print "Best estimator found by grid search:"
 print clf.best_estimator_
+print 'Parameter that gave the best results: ', clf.best_params_
 
 
 ###############################################################################
