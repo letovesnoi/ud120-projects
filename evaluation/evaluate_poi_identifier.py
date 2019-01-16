5bba9cb2ba0f266c80673c75ebedf771a5d8e554
letovesnoi <lenk.ff@gmail.com>
2019-01-16 17:56:07 +0300

    Back to code from the last lesson, number of predicted POIs for the test set (lesson 15)

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,19 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print "Accuracy:", accuracy_score(pred, labels_test)
+
+# How many POIs are predicted for the TEST set?
+print 'Number of predicted POIs: ', sum(labels_test)
 
 
